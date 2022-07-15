579efc0af3a64b3b471d1102d4c217aef7ccfab4
NILDARK <nilu.patel2002@gmail.com>
2022-07-15 23:49:45 +0530

    commit

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 import pymongo
-
+import datetime
 
 def addQuizAdmin(details):
     try:
@@ -27,6 +27,12 @@ def verifyUsername(username):
         print(err)
         return [False,None]
 if __name__=="__main__":
-    admins = db.quizAdmins
-    print(list(admins.find()))
+    client = pymongo.MongoClient("mongodb://quizDB:quizDB@cluster0-shard-00-00.jk81v.mongodb.net:27017,cluster0-shard-00-01.jk81v.mongodb.net:27017,cluster0-shard-00-02.jk81v.mongodb.net:27017/?ssl=true&replicaSet=atlas-rms0md-shard-0&authSource=admin&retryWrites=true&w=majority")
+    db = client.get_database('quiz')
+    sessions = db["sessions"]
+    myquery = { "session_owner": "test@123"}
+    newvalues = { "$set": { "session_start": datetime.datetime(year=2022, month=7, day=15,hour=23,minute=30) ,"session_end":datetime.datetime(year=2022, month=7, day=16,hour=10,minute=30) }}
 
+    sessions.update_one(myquery, newvalues)
+    for x in sessions.find(myquery):
+        print(x)
--- rough.py
+++ rough.py
@@ -1,7 +1,12 @@
 import rstr
 import re
+import datetime
 a = rstr.xeger(r'[A-Z]\d[A-Z]\d-[A-Z]\d[A-Z]\d/[A-Z,0-9]{6}')
 print(a)
+x = datetime.datetime(year=2022, month=7, day=15,hour=23,minute=14)
+print(x)
+# print(datetime.datetime.now())
+# print(datetime.datetime.now()+datetime.timedelta(minutes = 120))
 # b = "9a123456789"
 # x = re.search("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$",b)
 # # y = re.search("[0-9]",b)
